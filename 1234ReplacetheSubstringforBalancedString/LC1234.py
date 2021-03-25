9f404f4998e277cc9861f7b4ca05f0099cc388c7
Shu Shanggit config --global user.email shangshu1024@gmail.comgit config --listcleargit config --global user.name Shu <shangshu1024@gmail.com>
2021-03-25 16:25:05 -0400

    add 1234

---
+++ 1234ReplacetheSubstringforBalancedString/LC1234.py
@@ -0,0 +1,13 @@
+class Solution:
+    def balancedString(self, s: str) -> int:
+        
+        count = collections.Counter(s)
+        res = n = len(s)
+        i = 0
+        for j, c in enumerate(s):
+            count[c] -= 1
+            while i < n and all(n / 4 >= count[c] for c in 'QWER'):
+                res = min(res, j - i + 1)
+                count[s[i]] += 1
+                i += 1
+        return res
