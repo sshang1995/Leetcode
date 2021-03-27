2fa7634b8e2b0d3f3409bc5a05d428687797480d
Shu Shanggit config --global user.email shangshu1024@gmail.comgit config --listcleargit config --global user.name Shu <shangshu1024@gmail.com>
2021-03-27 12:25:05 -0400

    add approach 2

--- 1537GetMaxScore/LC1537.py
+++ 1537GetMaxScore/LC1537.py
@@ -16,3 +16,30 @@ class Solution:
 				j += 1
 		return max(a,b)% mod
 
+class Solution:
+    def maxSum(self, A: List[int], B: List[int]) -> int:
+        # Two pointers, 
+        # Max score = Max prefixsum(a,b)  + checkpoint + max postfixsum (a,b)
+        i, j, n, m = 0, 0, len(A), len(B)
+        a , b , mod = 0, 0, 10 ** 9 + 7
+        res = 0
+        while i < n and j < m:
+            if A[i] < B[j]:
+                a += A[i]
+                i += 1
+            elif A[i] > B[j]:
+                b += B[j]
+                j += 1
+            else:
+                res += max(a,b) + A[i]
+                i += 1
+                j += 1
+                a = 0
+                b = 0
+        while i < n:
+            a += A[i]
+            i +=1
+        while j < m:
+            b += B[j]
+            j += 1
+        return (res + max(a,b)) % mod
