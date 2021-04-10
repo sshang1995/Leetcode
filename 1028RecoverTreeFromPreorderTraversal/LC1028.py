b7edd969d4069e63117dd12e4427f3bb8e5b4137
Shu Shanggit config --global user.email shangshu1024@gmail.comgit config --listcleargit config --global user.name Shu <shangshu1024@gmail.com>
2021-04-10 19:15:59 -0400

    add LC1028(TreeNode)

---
+++ 1028RecoverTreeFromPreorderTraversal/LC1028.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def recoverFromPreorder(self, S: str) -> TreeNode:
+        stack, i = [], 0
+        while i < len(S):
+            level, val = 0, ""
+            while i < len(S) and S[i] == '-':
+                level += 1
+                i += 1
+            while i < len(S) and S[i] != '-':
+                val += S[i]
+                i += 1
+            # return back to previous root level
+            while len(stack) > level:
+                stack.pop()
+                
+            node = TreeNode(val)
+            
+            if stack and stack[-1].left is None:
+                stack[-1].left = node
+            elif stack:
+                stack[-1].right = node
+            # first append root node
+            stack.append(node)
+        return stack[0]
+        
