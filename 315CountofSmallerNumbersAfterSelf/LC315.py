c3ccc07491a023d2a57f880e46900ddfeb54370d
Shu Shanggit config --global user.email shangshu1024@gmail.comgit config --listcleargit config --global user.name Shu <shangshu1024@gmail.com>
2021-04-22 19:43:08 -0400

    add 315

---
+++ 315CountofSmallerNumbersAfterSelf/LC315.py
@@ -0,0 +1,37 @@
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        def sort(enum):
+            half = len(enum) // 2
+            if half:
+                left, right = sort(enum[:half]), sort(enum[half:])
+                for i in range(len(enum))[::-1]:
+                    if not right or left and left[-1][1] > right[-1][1]:
+                        smaller[left[-1][0]] += len(right)
+                        enum[i] = left.pop()      
+                    else:
+                        enum[i] = right.pop()
+                        
+            return enum
+        smaller = [0] * len(nums)
+        sort(list(enumerate(nums)))
+        return smaller
+
+def countSmaller(self, nums):
+    def sort(enum):
+        half = len(enum) / 2
+        if half:
+            left, right = sort(enum[:half]), sort(enum[half:])
+            m, n = len(left), len(right)
+            i = j = 0
+            while i < m or j < n:
+                if j == n or i < m and left[i][1] <= right[j][1]:
+                    enum[i+j] = left[i]
+                    smaller[left[i][0]] += j
+                    i += 1
+                else:
+                    enum[i+j] = right[j]
+                    j += 1
+        return enum
+    smaller = [0] * len(nums)
+    sort(list(enumerate(nums)))
+    return smaller
