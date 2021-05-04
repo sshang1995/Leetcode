c585729c5bb91bc70497620a784e089edc12d306
shu <shangshu1024@gmail.com>
2021-05-04 09:10:45 -0400

    add 55

---
+++ 55JumpGame/LC55.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        last = len(nums) -1
+        for i in range(len(nums)-2, -1, -1):
+            if i + nums[i] >= last:
+                last = i
+        return last <= 0
+
+# TC: O(N), SC: O(1)
